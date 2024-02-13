b1bf4ed0da250d37127a54fca35c91a4fa2d9142
James Li <zhiyueli.education@gmail.com>
2024-02-13 13:00:43 +0800

    Update data_process and visualisation at main.py file

--- data_process.py
+++ data_process.py
@@ -1,7 +1,24 @@
 
 import pandas as pd
 
-def data_process():
+def data_process(df):
+    # adjust column _date from object to datetime
+    cols = ['order_delivered_carrier_date','order_delivered_customer_date','order_estimated_delivery_date','shipping_limit_date','review_creation_date','order_purchase_timestamp']
+    for col in cols:
+        df[col] = pd.to_datetime(df[col])
+
+    # cleaning up name columns
+    # engineering new/essential columns
+    df['delivery_against_estimated'] = (df['order_estimated_delivery_date'] - df['order_delivered_customer_date']).dt.days
+    df['order_purchase_year'] = df['order_purchase_timestamp'].dt.year
+    df['order_purchase_month'] = df['order_purchase_timestamp'].dt.month
+    df['order_purchase_day'] = df['order_purchase_timestamp'].dt.day
+    df['order_purchase_dayofweek'] = df['order_purchase_timestamp'].dt.dayofweek
+    df['order_purchase_hr'] = df['order_purchase_timestamp'].dt.hour
+
+    return df
+
+def master():
     # access files
     path = 'dataset/'
     customers = pd.read_csv(path + 'olist_customers_dataset.csv')
@@ -15,18 +32,24 @@ def data_process():
     product_category = pd.read_csv(path + 'product_category_name_translation.csv')
 
     # Data Merge by relationship chart
+    # rename geolocation column geolocation_zip_code - > customer_zip_code
+    temp = geolocation.copy(deep = True)
+    temp.rename(columns = {'geolocation_zip_code_prefix':'customer_zip_code_prefix'},inplace = True)
+    customers_loc = customers.merge(temp, how ='inner', on = 'customer_zip_code_prefix')
+    del temp
+
     orders = order_dataset.merge(order_items, on = 'order_id', how = 'left')  # 1 to many relationship because 1 order might have multiple items.
     orders = orders.merge(order_payment, how = 'inner', on = 'order_id')
     orders = orders.merge(order_reviews, how = 'inner', on = 'order_id')
     orders = orders.merge(products, how = 'inner', on = 'product_id')
-    orders = orders.merge(sellers, how = 'inner', on = 'seller_id')
+
+    orders = orders.merge(product_category, how = 'inner', on = 'product_category_name')
 
 
-    geolocation.rename(columns = {'geolocation_zip_code_prefix':'customer_zip_code_prefix'},inplace = True)
-    customers_loc = customers.merge(geolocation, how ='inner', on = 'customer_zip_code_prefix')
+    # Except geolocation dataset
     customers_order = customers_loc.merge(orders, how = 'left', on = 'customer_id')
+    customers_order = data_process(customers_order)
 
-    customers_order = customers_order.merge(product_category, how = 'inner', on = 'product_category_name')
 
 
     return customers_order
