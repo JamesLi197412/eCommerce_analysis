0e2ade59da75fca25043c8826ec954967041fcf1
James Li <zhiyueli.education@gmail.com>
2024-04-23 21:48:34 +0800

    Write SQL for table schema

--- sql practice/commericial_analysis.sql
+++ sql practice/commericial_analysis.sql
@@ -1,3 +1,5 @@
+USE e_commerce;
+
 # Q1 Find out total sales and total orders
 SELECT COUNT(a.order_id) AS 'Total Orders', SUM(b.price + b.freight_value) AS 'Total Sales'
 FROM orders AS a
@@ -208,3 +210,104 @@ ON c.customer_id = a.customer_id
 WHERE order_status != 'available' AND order_status != 'canceled'
 GROUP BY customer_unique_id
 ORDER BY M DESC;
+
+# Q14 Find out daily active customer (DAU)
+SELECT t.day AS DAY, count(t.customer_unique_id) AS DAU
+FROM
+    (
+        SELECT date_format(o.order_purchase_timestamp, "%d") AS day,
+               c.customer_unique_id
+        FROM orders as o
+        LEFT JOIN customers as c
+        ON c.customer_id = o.customer_id
+        WHERE o.order_status = 'delivered' AND DATE_FORMAT(o.order_purchase_timestamp,"%Y-m") = '2017-09'
+        GROUP BY day, c.customer_unique_id
+    ) AS t
+GROUP BY DAY
+ORDER BY DAY;
+
+# Q15 Find out daily New User in Specific month/year e.g. 09/2017
+select DATE_FORMAT(temp.september,'%m-%d') as day, count(temp.new_customer) as daily_new_user
+from
+    (select c.customer_unique_id as new_customer, min(DATE_FORMAT(order_purchase_timestamp,'%Y-%m-%d')) as september
+     from orders as o
+     left join customers as c
+     on c.customer_id=o.customer_id
+     where DATE_FORMAT(order_purchase_timestamp,'%Y-%m')='2017-09'
+    group by c.customer_unique_id) as temp
+group by temp.september
+order by temp.september;
+
+# Q16 Re-Purchase
+select c.customer_unique_id as customer, min(DATE_FORMAT( o.order_purchase_timestamp,'%Y-%m-%d')) as first_purchase,
+       max(DATE_FORMAT(o.order_purchase_timestamp,'%Y-%m-%d')) as latest_purchase,
+       temp1.order_cnt as count_of_purchase, case when temp1.order_cnt<2 then 'N' else 'Y' end as repurchased,
+       coalesce(cast(avg_repurchase_cycle as integer), 'There was no repurchase') as average_rapurchase_cycle
+
+from orders as o
+
+        left join customers as c
+            on o.customer_id=c.customer_id
+        left join (
+                select cid, count(oid) as order_cnt
+                from (select c1.customer_unique_id as cid, o1.order_id as oid
+                    from orders as o1
+                    left join customers as c1
+                        on o1.customer_id=c1.customer_id
+                    where o1.order_status<> 'canceled'
+                    group by c1.customer_unique_id, o1.order_id) as t
+                group by cid) as temp1
+            on temp1.cid=c.customer_unique_id
+
+        left join order_items as i
+            on o.order_id=i.order_id
+
+        left join products as p
+            on p.product_id=i.product_id
+
+        left join product_category_name_translation as translate
+            on p.product_category_name=translate.product_category_name
+
+        left join (
+                select customer_unique_id, avg((order_purchase_timestamp)-(prev_purchase)) as avg_repurchase_cycle
+                from (select c2.customer_unique_id, o2.order_purchase_timestamp,
+                        lag(o2.order_purchase_timestamp) over(partition by c2.customer_unique_id order by o2.order_purchase_timestamp) as prev_purchase
+                    from orders as o2
+                    left join customers as c2
+                        on c2.customer_id=o2.customer_id
+                    where o2.order_status<> 'canceled')
+                where prev_purchase <> 'None'
+                group by customer_unique_id) as temp2
+            on temp2.customer_unique_id=c.customer_unique_id
+
+        where o.order_status<> 'canceled'
+        group by customer
+        order by count_of_purchase desc;
+
+# Q17 Growth rate by category
+        select
+            category,
+            coalesce(sum(case when period='2017_1st' then revenue end),'No record') as '2017_1st',
+            coalesce(sum(case when period='2018_1st' then revenue end),'No record') as '2018_1st',
+            round(sum(case when period='2018_1st' then revenue end)/sum(case when period='2017_1st' then revenue end)*100,2) as "growth_rate(%)"
+        from
+            (select ce.product_category_name_english as category,
+                case when DATE_FORMAT(o.order_purchase_timestamp,'%Y-%m') between '2017-01' and '2017-06' then '2017_1st'
+                    when DATE_FORMAT(o.order_purchase_timestamp,'%Y-%m') between '2018-01' and '2018-06' then '2018_1st' end as period,
+                    pay.payment_value as revenue
+            from order_payments as pay
+            left join orders as o
+                on pay.order_id=o.order_id
+            left join order_items as i
+                on i.order_id=o.order_id
+            left join products as p
+                on p.product_id=i.product_id
+            left join product_category_name_translation as ce
+                on p.product_category_name=ce.product_category_name
+
+            where
+                DATE_FORMAT( o.order_purchase_timestamp,'%Y-%m') between '2017-01' and '2017-06' or
+                DATE_FORMAT(o.order_purchase_timestamp,'%Y-%m') between '2018-01' and '2018-06' and
+                o.order_status not in ('canceled','unavailable')) as temp
+        group by category
+        order by "growth_rate(%)" desc;
