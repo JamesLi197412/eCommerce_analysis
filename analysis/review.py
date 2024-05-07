dd3b07c1f29ccdb008115b6d9a2388e05b74816b
James Li <zhiyueli.education@gmail.com>
2024-05-07 23:15:33 +0800

    Review analysis: TFIDF + BagofWord + Logistic Regression

--- analysis/review.py
+++ analysis/review.py
@@ -7,27 +7,32 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score
 import numpy as np
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfVectorizer
-
+from sklearn.feature_extraction.text import CountVectorizer,TfidfVectorizer
 
 # Sentiment analysis
 def review_analysis(reviews_df):
-    # stop_words = set(stopwords.words('portuguese'))
+    stop_words = set(stopwords.words('portuguese'))
     # tokenizer = RegexpTokenizer(r'\w+')
     # text process
     # 1. drop column with nan
     reviews_df.dropna(subset = ['review_comment_message'],inplace = True ) # Drop comment is NaN
 
-    reviews_df['typed_review_comment_message'] = reviews_df['review_comment_message'].apply(text_preprocessing)
-    print(reviews_df.head(5))
-    print('-' * 20)
-    print(reviews_df.columns)
+    reviews_df['tidy_review_comment_message'] = reviews_df['review_comment_message'].apply(text_preprocessing)
+    reviews_df['label'] = np.where(reviews_df['review_score'] >=3 , 1, 0)
 
-    common_words_visualisation(reviews_df,'typed_review_comment_message')
+    # common_words_visualisation(reviews_df,'typed_review_comment_message')
 
-    # Extracting features from cleaned reviews
-    # Bag of Words
+    # Bag of Words + Logistic Regression
+    test_df = bag_of_words(reviews_df,stop_words) # generate the prediction
+    test_df.columns = ['review_id','prediction_label']
+    # print(test_df.columns)
+    # merge test_df with original review_df
+    test_df = test_df.merge(reviews_df, on = 'review_id', how = 'inner')
+    print(test_df.head(5))
+    print(test_df.columns)
+
+    # TF-IDF + Logistic Regression
+    # tf_idf(reviews_df,stop_words)
 
 
     return reviews_df
@@ -42,6 +47,7 @@ def text_preprocessing(data):
     words = tokenizer.tokenize(txt)     # Tokenization
     words = [w for w in words if not w in stop_words]  # remove stop words
     words = [stemmer.stem(i) for i in words]  # stemming the word
+    words = ','.join([text for text in words])
 
     return words
 
@@ -54,42 +60,52 @@ def common_words_visualisation(df,col):
     plt.axis('off')
     plt.savefig(f'output/visualisation/reviews/common words.png')
 
-def bag_of_words(df,col):
-    review_vectorizer = CountVectorizer(max_df=0.90, min_df=2, max_features=1000, stop_words='portuguese')
-    reviews = review_vectorizer.fit_transform(df[col])
-    train_bow = reviews[:31962, :]
-    test_bow = reviews[31962:, :]
+def bag_of_words(df, stop_words,col = 'tidy_review_comment_message'):
+    sizes = int(round(df.shape[0]) * 0.8)
+    train = df.iloc[:sizes,:]
+    test = df.iloc[sizes:,:]
+
+    review_vectorizer = CountVectorizer(max_df=0.90, min_df=2, max_features=1000, stop_words=stop_words)
+    # bag-of-words feature matrix
+    reviews_train = review_vectorizer.fit_transform(train[col])
+    reviews_test = review_vectorizer.fit_transform(test[col])
 
     # splitting data into training and validation set
-    xtrain_bow, xvalid_bow, ytrain, yvalid = train_test_split(train_bow, train['label'], random_state=42, test_size=0.3)
+    xtrain, xvalid, ytrain, yvalid = train_test_split(reviews_train, train['label'], random_state=42, test_size=0.3)
 
     lreg = LogisticRegression()
-    lreg.fit(xtrain_bow, ytrain)  # training the model
+    lreg.fit(xtrain, ytrain)  # training the model
 
-    prediction = lreg.predict_proba(xvalid_bow)  # predicting on the validation set
+    prediction = lreg.predict_proba(xvalid)  # predicting on the validation set
     prediction_int = prediction[:, 1] >= 0.5  # if prediction is greater than or equal to 0.3 than 1 else 0
-    prediction_int = prediction_int.astype(np.int)
+    prediction_int = prediction_int.astype(int)
 
     f1_score(yvalid, prediction_int)  # calculating f1 score
 
-    test_pred = lreg.predict_proba(test_bow)
+    test_pred = lreg.predict_proba(reviews_test)
     test_pred_int = test_pred[:, 1] >= 0.3
-    test_pred_int = test_pred_int.astype(np.int)
-    test['label'] = test_pred_int
-    submission = test[['id', 'label']]
-    submission.to_csv('sub_lreg_bow.csv', index=False)  # writing data to a CSV file
+    test_pred_int = test_pred_int.astype(int)
+    test.loc[:, 'label'] = test_pred_int
+    submission = test[['review_id', 'label']]
+    # submission.to_csv('output/review_bag_of_word.csv', index=False)  # writing data to a CSV file
+    return submission
 
-def tf_idf(df,col):
-    tfidf_vectorizer = TfidfVectorizer(max_df=0.90, min_df=2, max_features=1000, stop_words='english')
+def tf_idf(df,stop_words, col = 'tidy_review_comment_message'):
+    sizes = int(round(df.shape[0]) * 0.8)
+    train = df.iloc[:sizes,:]
+    test = df.iloc[sizes:,:]
+
+    tfidf_vectorizer = TfidfVectorizer(max_df=0.90, min_df=2, max_features=1000, stop_words=stop_words)
     # TF-IDF feature matrix
-    tfidf = tfidf_vectorizer.fit_transform(df[col])
+    train_tfidf = tfidf_vectorizer.fit_transform(train[col])
+    test_tfidf = tfidf_vectorizer.fit_transform(test[col])
 
-    train_tfidf = tfidf[:31962, :]
-    test_tfidf = tfidf[31962:, :]
+    xtrain, xvalid, ytrain, yvalid = train_test_split(train_tfidf, train['label'], random_state=42, test_size=0.3)
 
     xtrain_tfidf = train_tfidf[ytrain.index]
     xvalid_tfidf = train_tfidf[yvalid.index]
 
+    lreg = LogisticRegression()
     lreg.fit(xtrain_tfidf, ytrain)
 
     prediction = lreg.predict_proba(xvalid_tfidf)
